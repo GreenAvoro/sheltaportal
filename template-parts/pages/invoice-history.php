879ccedde5d7f2f5caf8b82fe0e8ea61a582b5dd
GreenAvoro <haydos.shaw@gmail.com>
2022-10-28 10:53:38 +1300

    update

--- template-parts/pages/invoice-history.php
+++ template-parts/pages/invoice-history.php
@@ -4,8 +4,14 @@ ob_start();
 
 
 
-<div>
-    <h1>This is the Invoice History page</h1>
+<div class="portal-container layout-1-3">
+    <div class="panels-column">
+        <?= get_template_part('template-parts/navigation'); ?>
+    </div>
+    <div class="panels-column">
+        <h1>This is the panel</h1>
+    </div>
+
 </div>
 
 
